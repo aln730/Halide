bbddb5a0d28a263fcfc6ba5cb3b596a6f5f52050
Arnav Gawas <arnavgawas707@gmail.com>
2024-11-21 01:34:43 -0500

    add rich

--- CLIotify.py
+++ CLIotify.py
@@ -1,15 +1,12 @@
-"""
-Author: zxcv@csh/asg7201
-Description: CLI-based Music player using Python3, PyGame, Random, OS
-PS: Don't mind some of the the crappy code. 
-TM: expand the music library and add an option to create playlists.
-"""
 import os
 import pygame
-from colorama import init, Fore
-from termcolor import colored
+from rich.console import Console
+from rich.color import Color
+from rich.text import Text
+from rich.prompt import Prompt
+from rich.panel import Panel
 
-init()
+console = Console()
 
 class MusicPlayer:
     def __init__(self):
@@ -27,28 +24,35 @@ class MusicPlayer:
                 self.songs.append(os.path.join(folder_path, filename))
 
     def list_songs(self):
-        print("Available songs:")
+        console.print("Available songs:", style="bold magenta")
         for i, song in enumerate(self.songs):
-            print(f"{i+1}. {song}")
+            console.print(f"{i + 1}. {song}", style="dim")
 
     def play_song(self, song_index):
         if song_index.isdigit() and 0 <= int(song_index) - 1 < len(self.songs):
             pygame.mixer.music.load(self.songs[int(song_index) - 1])
             pygame.mixer.music.play()
             self.current_song_index = int(song_index) - 1
+            console.print(f"Now playing: {self.songs[self.current_song_index]}", style="dim")
         else:
-            print("Invalid song index.")
+            console.print("[red]Invalid song index.[/red]")
 
     def pause_song(self):
-        if not self.paused:
-            pygame.mixer.music.pause()
-            self.paused = True
+        if pygame.mixer.music.get_busy():
+            if not self.paused:
+                pygame.mixer.music.pause()
+                self.paused = True
+                console.print("[yellow]Song paused.[/yellow]")
+            else:
+                pygame.mixer.music.unpause()
+                self.paused = False
+                console.print("[yellow]Song resumed.[/yellow]")
         else:
-            pygame.mixer.music.unpause()
-            self.paused = False
+            console.print("[red]No song is currently playing.[/red]")
 
     def stop_song(self):
         pygame.mixer.music.stop()
+        console.print("[red]Song stopped.[/red]")
 
     def next_song(self):
         if len(self.songs) > 1:
@@ -66,6 +70,7 @@ class MusicPlayer:
 
     def seek_song(self, seconds):
         pygame.mixer.music.set_pos(seconds)
+        console.print(f"[cyan]Seeked to {seconds} seconds.[/cyan]")
 
     def shuffle_songs(self):
         import random
@@ -73,42 +78,48 @@ class MusicPlayer:
         self.play_song("1")
 
     def run(self):
-        print("CLIotify - root@zxcv\n")
-        ascii ="""⠀⠀⠀⠀⠀⠀⠀⠀⣾⣛⡿⡿⢽⣶⣤⣀⣀⣀⣀⣀⣀⣠⣴⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢠⣿⢬⡭⢭⣭⣽⣗⡋⠈⠉⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⡞⠁⣂⡠⠾⢛⣛⣀⣀⠀⠀⠙⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣟⠛⠃⡀⣀⡉⠀⠄⠄⠈⠉⠑⠢⣄⡉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠈⠲⣍⣿⣿⣿⣿⣿⣿⣿⡉⠛⢿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⢿⣿⣖⠿⣯⢆⠀⠀⢁⠞⠉⠙⠻⠿⣿⣿⣿⣿⣦⣀⣩⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣤⡄⠀⠀⠀⢀⡴⠛⢓⡜⢿⣿⣆⢀⢿⠀⠀⢻⣄⡀⠀⠀⠀⢒⣛⣫⣿⡟⠻⢿⠿⣾⡻⣿⡆⠀⠀⠀⠀⠀⠀⠀
-⢀⡀⢤⣤⢾⣿⣷⠄⢀⡠⠈⣄⠤⠀⠛⢾⣿⣿⣾⡎⠀⠀⠀⠈⠛⠒⠀⠀⠀⠀⠀⢈⣑⡒⠤⠄⣀⣈⣭⠗⠀⠀⠀⠀⠀⠀⠀
-⡏⠈⠉⠉⣿⣿⣿⠍⠹⣿⡄⠉⠻⢶⣌⠀⠻⣿⣿⣿⣷⣶⣴⣾⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣦⣄⣒⣊⠀⠀⠀⠀⠀⠀⠀⠀
-⠹⡦⡁⠀⠸⡟⣿⣤⢬⢿⠀⠀⠀⠀⠉⡷⣄⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣶⣤⣤⣀⠀⠀
-⠀⠙⢾⡴⠄⡀⢸⣿⡏⠁⠀⠀⠀⠀⠀⠈⠪⡓⢤⡈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣷⣿⣟⢵⣆⣈⣍⣿⣿⣿⣿⣷⡄
-⠀⠀⠀⢷⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠈⠂⢌⡓⠢⢤⣉⠉⠛⠻⢿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⠿⠛⠛⠛⠛⢛⣨⣿⡗
-⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠲⠤⠍⣒⡂⠀⢼⡉⠛⠁⠀⠀⠀⣠⠟⠻⠙⠉⠀⠁⣉⣁⡠⠔⠀
-⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠒⡇⠀⠀⠀⠀⠈⢀⡠⠔⠒⠀⠉⠉⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢹⡘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡿⣕⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⠤⠄⠰⠤⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢳⡎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣺⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢻⣿⣾⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⡶⠏⠉⠻⢿⣿⣿⣿⣿⣿⡷⠆⠙⠿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠘⣄⠀⠀⠀⠈⣛⣿⡿⠟⢿⣷⣮⣸⡐⠛⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠙⠯⢒⣲⠄⠀⠀⠀⢄⣠⠀⠈⠿⣿⡀⠀⢻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠐⠲⢌⣓⠤⣀⠀⠀⠀⠈⢳⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠂⠭⠝⣒⠻⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀      
-        """
-        print(colored((ascii),color = "magenta"))
-        folder_path = input("Enter the folder: ")
+        centered_text = "CLIotify Music Player".center(85)
+        console.print(Panel(centered_text, style="bold magenta",width=89))
+        ascii_art = """                    ⠀⠀⠀⠀⠀⠀⠀⠀⣾⣛⡿⡿⢽⣶⣤⣀⣀⣀⣀⣀⣀⣠⣴⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⢠⣿⢬⡭⢭⣭⣽⣗⡋⠈⠉⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⡞⠁⣂⡠⠾⢛⣛⣀⣀⠀⠀⠙⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⣟⠛⠃⡀⣀⡉⠀⠄⠄⠈⠉⠑⠢⣄⡉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠈⠲⣍⣿⣿⣿⣿⣿⣿⣿⡉⠛⢿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⢿⣿⣖⠿⣯⢆⠀⠀⢁⠞⠉⠙⠻⠿⣿⣿⣿⣿⣦⣀⣩⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⣤⡄⠀⠀⠀⢀⡴⠛⢓⡜⢿⣿⣆⢀⢿⠀⠀⢻⣄⡀⠀⠀⠀⢒⣛⣫⣿⡟⠻⢿⠿⣾⡻⣿⡆⠀⠀⠀⠀⠀⠀⠀
+                    ⢀⡀⢤⣤⢾⣿⣷⠄⢀⡠⠈⣄⠤⠀⠛⢾⣿⣿⣾⡎⠀⠀⠀⠈⠛⠒⠀⠀⠀⠀⠀⢈⣑⡒⠤⠄⣀⣈⣭⠗⠀⠀⠀⠀⠀⠀⠀
+                    ⡏⠈⠉⠉⣿⣿⣿⠍⠹⣿⡄⠉⠻⢶⣌⠀⠻⣿⣿⣿⣷⣶⣴⣾⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣦⣄⣒⣊⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠹⡦⡁⠀⠸⡟⣿⣤⢬⢿⠀⠀⠀⠀⠉⡷⣄⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣶⣤⣤⣀⠀⠀
+                    ⠀⠙⢾⡴⠄⡀⢸⣿⡏⠁⠀⠀⠀⠀⠀⠈⠪⡓⢤⡈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣷⣿⣟⢵⣆⣈⣍⣿⣿⣿⣿⣷⡄
+                    ⠀⠀⠀⢷⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠈⠂⢌⡓⠢⢤⣉⠉⠛⠻⢿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⠿⠛⠛⠛⠛⢛⣨⣿⡗
+                    ⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠲⠤⠍⣒⡂⠀⢼⡉⠛⠁⠀⠀⠀⣠⠟⠻⠙⠉⠀⠁⣉⣁⡠⠔⠀
+                    ⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠒⡇⠀⠀⠀⠀⠈⢀⡠⠔⠒⠀⠉⠉⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⢹⡘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡿⣕⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⠤⠄⠰⠤⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⢳⡎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣺⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⢻⣿⣾⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⢀⡶⠏⠉⠻⢿⣿⣿⣿⣿⣿⡷⠆⠙⠿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠘⣄⠀⠀⠀⠈⣛⣿⡿⠟⢿⣷⣮⣸⡐⠛⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠙⠯⢒⣲⠄⠀⠀⠀⢄⣠⠀⠈⠿⣿⡀⠀⢻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠐⠲⢌⣓⠤⣀⠀⠀⠀⠈⢳⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠂⠭⠝⣒⠻⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    """
+        console.print(Text(ascii_art, style="bold magenta"  ))
+
+        folder_path = Prompt.ask("Enter the folder path")
         self.read_songs_from_folder(folder_path)
-        print(colored("Fplder Initialized", color="magenta"))
+        console.print("[magenta]Folder Initialized[/magenta]")
+        
         while True:
-            print(colored("""\nOptions: [l]ist songs, [p]lay, [pause/resume], [s]top, [n]ext\n[b]ack shuffle, [f]orward, [r]everse, [q]uit""", color="magenta"))
-            choice = input(colored("Choose an option: ", color="magenta")).lower()
+            console.print("[magenta]Options: list songs, play, pause/resume, stop, nextback shuffle, forward, reverse, quit[/magenta]")
+            choice = Prompt.ask("Choose an option", choices=["l", "p", "pause", "resume", "s", "n", "b", "f", "r", "shuffle", "q"])
+
             if choice == 'l':
                 self.list_songs()
             elif choice == 'p' or choice == 'play':
-                song_index = input("Enter song number to play: ")
+                song_index = Prompt.ask("Enter song number to play")
                 self.play_song(song_index)
+            elif choice == 'pause' or choice == 'resume':
+                self.pause_song()
             elif choice == 's' or choice == 'stop':
                 self.stop_song()
             elif choice == 'n' or choice == 'next':
@@ -116,17 +127,18 @@ class MusicPlayer:
             elif choice == 'b' or choice == 'back':
                 self.previous_song()
             elif choice == 'f' or choice == 'forward':
-                seconds = int(input("Enter the number of seconds to skip forward: "))
+                seconds = Prompt.ask("Enter the number of seconds to skip forward", type=int)
                 self.seek_song(seconds)
             elif choice == 'r' or choice == 'reverse':
-                seconds = int(input("Enter the number of seconds to skip backward: "))
+                seconds = Prompt.ask("Enter the number of seconds to skip backward", type=int)
                 self.seek_song(-seconds)
             elif choice == 'shuffle':
                 self.shuffle_songs()
             elif choice == 'q' or choice == 'quit':
                 break
             else:
-                print("Invalid option. Please try again.")
+                console.print("[red]Invalid option. Please try again.[/red]")
+
 
 if __name__ == "__main__":
     music_player = MusicPlayer()
